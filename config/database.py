c17f36c8df5f7de68f65b0c6cf778c88888cbe10
kombarov <kombarov@itopcase.ru>
2022-12-16 21:34:44 +0300

    обратно на sqlite

--- config/database.py
+++ config/database.py
@@ -2,11 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# DATABASE_URL = "sqlite:///../wb-backend/sql_app.db"
-DATABASE_URL = "postgresql://nr:nr@localhost:5432/wbv"
+from config import app_config
+
+DATABASE_URL = "sqlite:///{}".format(app_config.db_path)
+# DATABASE_URL = "postgresql://nr:nr@localhost:5432/wbv"
 
 engine = create_engine(
-    DATABASE_URL
+    DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
 session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
